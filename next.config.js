7e4d7bc42d3eec8862e36ea7cf1bd5087aa2caa7
wypratama <wicaksanapratama@gmail.com>
2022-02-01 14:23:56 +0700

    feat(init): add antdesign to dependency

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  serverRuntimeConfig: {
+    apiUrl: process.env.API_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
