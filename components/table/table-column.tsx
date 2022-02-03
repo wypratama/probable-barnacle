d27bedfffe1e7754cbba251778a7d6bac2266c0e
wypratama <wicaksanapratama@gmail.com>
2022-02-03 07:22:28 +0700

    disable crud function without login

--- components/table/table-column.tsx
+++ components/table/table-column.tsx
@@ -15,53 +15,57 @@ export default function ColumnsOfTable() {
     [loading, setLoading] = useState(false),
     setUser = useStore((state) => state.setUser),
     fetchData = useStore((state) => state.fetchData),
+    loggedUser = useStore((state) => state.loggedUser),
     confirmDelete = (item: ItemsWithKey) => {
-      Modal.confirm({
-        title: `Deleting ${item.product_name}`,
-        content: `Are you sure you want to delete ${item.product_name}`,
-        centered: true,
-        okText: 'Delete',
-        okType: 'danger',
-        onOk: async () => {
-          const token = localStorage.getItem('token');
-          try {
-            setLoading(true);
-            const response = await fetch('/api/delete', {
-                method: 'POST',
-                headers: {
-                  Authorization: `Bearer ${token}`,
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ sku: item.sku }),
-              }),
-              parsed = await response.json();
-            if (!response.ok) throw { ...parsed, type: 'token' };
-            if (parsed.message) throw { ...parsed, type: 'failure' };
-            message.success(`Success deleting ${item.product_name}`);
-            fetchData();
-            console.log({ sku: item.sku }, response, 'dari submit function');
-          } catch (error: any) {
-            console.log(error, 'error submit');
-            if (error.type === 'token')
-              Modal.confirm({
-                title: 'Your login token is expired',
-                content: 'you need to re-login to add product',
-                onCancel: () => {
-                  router.push('/');
-                },
-                onOk: () => {
-                  localStorage.removeItem('token');
-                  setUser(null);
-                  router.push('/login');
-                },
-              });
-            if (error.type === 'failure') {
-              message.error(`Product with ${item.sku} not found`);
+      if (!loggedUser)
+        message.error('You need to login'), router.push('/login');
+      else
+        Modal.confirm({
+          title: `Deleting ${item.product_name}`,
+          content: `Are you sure you want to delete ${item.product_name}`,
+          centered: true,
+          okText: 'Delete',
+          okType: 'danger',
+          onOk: async () => {
+            const token = localStorage.getItem('token');
+            try {
+              setLoading(true);
+              const response = await fetch('/api/delete', {
+                  method: 'POST',
+                  headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({ sku: item.sku }),
+                }),
+                parsed = await response.json();
+              if (!response.ok) throw { ...parsed, type: 'token' };
+              if (parsed.message) throw { ...parsed, type: 'failure' };
+              message.success(`Success deleting ${item.product_name}`);
+              fetchData();
+              console.log({ sku: item.sku }, response, 'dari submit function');
+            } catch (error: any) {
+              console.log(error, 'error submit');
+              if (error.type === 'token')
+                Modal.confirm({
+                  title: 'Your login token is expired',
+                  content: 'you need to re-login to add product',
+                  onCancel: () => {
+                    router.push('/');
+                  },
+                  onOk: () => {
+                    localStorage.removeItem('token');
+                    setUser(null);
+                    router.push('/login');
+                  },
+                });
+              if (error.type === 'failure') {
+                message.error(`Product with ${item.sku} not found`);
+              }
             }
-          }
-          setLoading(false);
-        },
-      });
+            setLoading(false);
+          },
+        });
     };
 
   return [
@@ -140,7 +144,11 @@ export default function ColumnsOfTable() {
                   style={{ fontSize: '20px' }}
                 />
               }
-              onClick={() => router.push(`/edit/${item.sku}`)}
+              onClick={() => {
+                if (!loggedUser)
+                  message.error('You need to login'), router.push('/login');
+                else router.push(`/edit/${item.sku}`);
+              }}
             ></Button>
             <Button
               shape='circle'
--- components/table/table-header.tsx
+++ components/table/table-header.tsx
@@ -1,17 +1,21 @@
 import { PlusOutlined } from '@ant-design/icons';
-import { Button, Input } from 'antd';
+import { Button, Input, message } from 'antd';
 import { useRouter } from 'next/router';
 import styles from '../../styles/Table.module.scss';
+import useStore from '../../utils/store';
 
 export default function tableHeader(
   searchFunction: (val: string) => void,
   changeFunction: (e: any) => void
 ) {
   return function useHeader() {
-    const router = useRouter();
-    const onAddClick = () => {
-      router.push('/add');
-    };
+    const router = useRouter(),
+      loggedUser = useStore((state) => state.loggedUser),
+      onAddClick = () => {
+        if (!loggedUser)
+          message.error('You need to login'), router.push('/login');
+        else router.push('/add');
+      };
     return (
       <div className={styles.header}>
         <Button onClick={onAddClick}>
